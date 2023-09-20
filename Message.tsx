8b1da60c917b2ffd8b9e3b38fc72c029e2c4e165
iremberber <iremberber0@gmail.com>
2023-09-20 23:32:56 +0300

    React 1

---
+++ Message.tsx
@@ -0,0 +1,13 @@
+
+//PascalCasing
+function Message() {
+    //JSX: Javascript XML
+
+    const name = 'furkan';
+    if(name)
+  return <h1>Hello World {name}</h1>;
+}
+
+export default Message;
+
+
