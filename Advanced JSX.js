8fa42530e559071c62f3f6ade5cd3a729e01c39b
iremberber <iremberber0@gmail.com>
2024-04-08 05:41:52 +0300

    Update Advanced JSX.js

--- Advanced JSX.js
+++ Advanced JSX.js
@@ -1,5 +1,28 @@
+//1 class vs className
 import React from 'react';
 import { createRoot } from 'react-dom/client'
 
 const container = document.getElementById('app');
 const root = createRoot(container);
+
+const myDiv = <div className="big">I AM A BIG DIV</div>
+root.render(myDiv)
+
+//2 Self-Closing Tags
+
+const profile = (
+    <div>
+      <h1>John Smith</h1>
+      <img src="images/john.png"/>
+      <article>
+        My name is John Smith.
+        <br/>
+        I am a software developer.
+        <br/>
+        I specialize in creating React applications.
+      </article>
+    </div>
+  );
+
+  
+
