3c2814b802590227142c6ec353e365ae5b6321a5
iremberber <iremberber0@gmail.com>
2024-04-19 17:18:20 +0300

    Update Advanced JSX.js

--- Advanced JSX.js
+++ Advanced JSX.js
@@ -52,7 +52,68 @@ const gooseImg = (
 );
 root.render(gooseImg);
 
-//Event Listeners in JSX
+//8 Event Listeners in JSX
+
+
+function makeDoggy(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+const kitty = (
+	<img onClick={makeDoggy}
+		src="https://content.codecademy.com/courses/React/react_photo-kitty.jpg" 
+		alt="kitty" />
+);
+
+root.render(kitty);
+
+//9 JSX Conditionals: If Statements That Don't Work
+
+/*
+You can not inject an if statement into a JSX expression.
+This code will break
+(
+  <h1>
+    {
+      if (purchase.complete) {
+        'Thank you for placing an order!'
+      }
+    }
+  </h1>
+)
+*/
+
+//10 JSX Conditionals: If Statements That Do Work
+
+function coinToss() {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+let img;
+
+// if/else statement begins here:
+if(coinToss() === "heads"){
+  img = <img src={pics.kitty} />
+} else{
+  img = <img src={pics.doggy} />
+}
+
+root.render(img);
+
+//11 JSX Conditionals: The Ternary Operator
+
+
+
+
+
 
 
 
