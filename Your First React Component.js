2fbbd45f0f126d7353ffa416fee0819a9facea39
iremberber <iremberber0@gmail.com>
2024-04-29 23:26:58 +0300

    Create Your First React Component.js

---
+++ Your First React Component.js
@@ -0,0 +1,26 @@
+//1 React Components
+
+import React from 'react';
+
+function App() {
+  return <h1>Hello again, World!</h1>;
+}
+
+export default App;
+
+//2 Import React
+//app.js
+import React from 'react';
+//index.js
+import React from 'react';
+
+//3 Import ReactDOM
+//index.js
+import ReactDOM from 'react-dom/client';
+
+//4 Create a Function Component
+//app.js
+function IremsComponent(){
+}
+
+
