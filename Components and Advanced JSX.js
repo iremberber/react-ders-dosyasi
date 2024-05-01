031bd3a9c27ced40343fcf0e8fb97f09ed6ae3aa
iremberber <iremberber0@gmail.com>
2024-05-02 00:36:32 +0300

    Create Components and Advanced JSX.js

---
+++ Components and Advanced JSX.js
@@ -0,0 +1,22 @@
+//1 Use Multiline JSX in a Component
+
+import React from 'react';
+
+function QuoteMaker() {
+    return (
+      <blockquote>
+        <p>
+           What is important now is to recover our senses.
+        </p>
+        <cite>
+          <a target="_blank" 
+          href="https://en.wikipedia.org/wiki/Susan_Sontag">
+           Susan Sontag
+          </a>
+        </cite>
+       </blockquote>
+    );
+};
+
+export default QuoteMaker;
+
