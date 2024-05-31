cf020a4928431989520491bffc72d9f5303eab0a
sevda <iremberber0@gmail.com>
2024-05-31 23:46:58 +0300

    Update Props.js

---
+++ Props.js
@@ -0,0 +1,15 @@
+//2 Access a Component's props
+
+import React from 'react';
+
+function PropsDisplayer(props) {
+  const stringProps = JSON.stringify(props);
+  return (
+    <div>
+      <h1>CHECK OUT MY PROPS OBJECT</h1>
+      <h2>{stringProps}</h2>
+    </div>
+  );
+}
+
+export default PropsDisplayer;
