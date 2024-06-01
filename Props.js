9dd99fd57374eb16c22e7aee8fcae5a5520e3970
sevda <iremberber0@gmail.com>
2024-06-01 23:40:23 +0300

    Update Props.js

--- Props.js
+++ Props.js
@@ -1,6 +1,7 @@
 //2 Access a Component's props
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 function PropsDisplayer(props) {
   const stringProps = JSON.stringify(props);
@@ -13,3 +14,27 @@ function PropsDisplayer(props) {
 }
 
 export default PropsDisplayer;
+
+//3 Pass `props` to a Component
+
+import PropsDisplayer from './PropsDisplayer';
+
+function App() {
+  return <PropsDisplayer myProp="Hello" />;
+}
+
+export default App;
+
+//4 Render a Component's props
+
+function Product(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.price}</h2>
+      <h3>{props.rating}</h3>
+    </div>       
+  );
+}
+
+export default Product;
