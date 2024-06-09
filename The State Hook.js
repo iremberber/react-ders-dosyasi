4066d3b64a51a9004f3e05602f5a00c88f65aff4
sevda <iremberber0@gmail.com>
2024-06-09 18:43:18 +0300

    Update The State Hook.js

--- The State Hook.js
+++ The State Hook.js
@@ -64,3 +64,29 @@ export default function ColorPicker() {
     </div>
   );
 }
+
+//3 Initialize State
+
+import React, { useState } from 'react';
+
+const colorNames = ['Aquamarine', 'BlueViolet', 'Chartreuse', 'CornflowerBlue', 'Thistle', 'SpringGreen', 'SaddleBrown', 'PapayaWhip', 'MistyRose'];
+
+export default function ColorPicker() {
+  const [color, setColor] = useState("Tomato");
+
+ const divStyle = {backgroundColor: color};
+
+  return (
+    <div style={divStyle}>
+      <p>Selected color: {color}</p>
+      {colorNames.map((colorName)=>(
+        <button 
+          onClick={() => setColor(colorName)} 
+          key={colorName}>
+       	     {colorName}
+      	</button>
+      ))}
+    </div>
+  );
+}
+
