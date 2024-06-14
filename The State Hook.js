4383e7e652d4362a2541b1b9007b5c5a711ca0df
sevda <iremberber0@gmail.com>
2024-06-14 19:49:45 +0300

    Update The State Hook.js

--- The State Hook.js
+++ The State Hook.js
@@ -240,4 +240,37 @@ export default function EditProfile() {
   );
 }
 
+//7 Separate Hooks for Separate States
+
+import React, { useState } from "react";
+
+function Musical() {
+   const [state, setState] = useState({
+    title: "Best Musical Ever",
+    actors: ["George Wilson", "Tim Hughes", "Larry Clements"],
+    locations: {
+      Chicago: {
+        dates: ["1/1", "2/2"], 
+        address: "chicago theater"}, 
+      SanFrancisco: {
+        dates: ["5/2"], 
+        address: "sf theater"
+      }
+    }
+  })
+ }
+
+function MusicalRefactored() {
+  const [title, setTitle]= useState("Best Musical Ever");
+  const [actors, setActors] = useState(["George Wilson", "Tim Hughes", "Larry Clements"]);
+  const [locations, setLocations] = useState({
+      Chicago: {
+        dates: ["1/1", "2/2"], 
+        address: "chicago theater"}, 
+      SanFrancisco: {
+        dates: ["5/2"], 
+        address: "sf theater"
+      });
+}
+
 
