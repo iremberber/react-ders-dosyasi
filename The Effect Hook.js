3395bc330e5e9d264a7ab85e3eb417fb0b1aa5d2
sevda <iremberber0@gmail.com>
2024-07-08 23:40:16 +0300

    Clean Up Effects

--- The Effect Hook.js
+++ The Effect Hook.js
@@ -24,3 +24,24 @@ export default function Counter() {
     </div>
   );
 }
+
+//2 Clean Up Effects
+
+import React, { useState, useEffect } from 'react';
+
+export default function Counter() {
+  const [clickCount, setClickCount] = useState(0);
+
+  const increment = () => setClickCount((prev) => prev + 1);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', increment);
+    return () => {
+      document.removeEventListener('mousedown', increment);
+    };
+  });
+
+  return (
+      <h1>Document Clicks: {clickCount}</h1>
+  );
+}
