fc50c85624049ed1475249aaba1c842bc506799d
sevda <iremberber0@gmail.com>
2024-07-10 22:16:03 +0300

    Update The Effect Hook.js

--- The Effect Hook.js
+++ The Effect Hook.js
@@ -45,3 +45,97 @@ export default function Counter() {
       <h1>Document Clicks: {clickCount}</h1>
   );
 }
+
+// 3 Control When Effects Are Called
+
+import React, { useState, useEffect } from 'react';
+
+export default function Timer() {
+  const [time, setTime] = useState(0);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const handleChange = ({ target }) => setName(target.value);
+
+  return (
+    <>
+      <h1>Time: {time}</h1>
+      <input value={name} onChange={handleChange} type='text' />
+    </>
+  );
+}
+
+//4 Fetch Data
+
+
+import { get } from './mockBackend/fetch';
+
+export default function Forecast() {
+  const [data, setData] = useState(null);
+  const [notes, setNotes] = useState({});
+  const [forecastType, setForecastType] = useState('/daily');
+
+  useEffect(() => {
+    alert('Requested data from server...');
+    get(forecastType).then((response) => {
+      alert('Response: ' + JSON.stringify(response,'',2));
+      setData(response.data);
+    });
+  }, [forecastType]);
+
+  const handleChange = (itemId) => ({ target }) =>
+    setNotes((prev) => ({
+      ...prev,
+      [itemId]: target.value
+    }));
+
+  if (!data) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className='App'>
+      <h1>My Weather Planner</h1>
+      <div>
+        <button onClick={() => setForecastType('/daily')}>5-day</button>
+        <button onClick={() => setForecastType('/hourly')}>Today</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Summary</th>
+            <th>Avg Temp</th>
+            <th>Precip</th>
+            <th>Notes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td>{item.summary}</td>
+                <td> {item.temp.avg}°F</td>
+                <td>{item.precip}%</td>
+                <td>
+                  <input
+                    value={notes[item.id] || ''}
+                    onChange={handleChange(item.id)}
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
