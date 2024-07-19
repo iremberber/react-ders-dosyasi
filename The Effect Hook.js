6bf19e3c409fc208708cca831a4426a22c1239dd
sevda <iremberber0@gmail.com>
2024-07-19 19:46:46 +0300

    Update The Effect Hook.js

--- The Effect Hook.js
+++ The Effect Hook.js
@@ -139,3 +139,124 @@ export default function Forecast() {
     </div>
   );
 }
+
+//5 Rules of Hooks
+
+import React, { useState, useEffect } from 'react';
+import { get } from './mockBackend/fetch';
+
+export default function Shop() {
+  const [categories, setCategories] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [items, setItems] = useState({});
+
+  useEffect(() => {
+    get('/categories').then((response) => {
+      setCategories(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (selectedCategory && !items[selectedCategory]) {
+      get(`/items?category=${selectedCategory}`).then((response) => {
+        setItems((prev) => ({ ...prev, [selectedCategory]: response.data }));
+      });
+    }
+  }, [items, selectedCategory]);
+
+  if (!categories) {
+    return <p>Loading..</p>;
+  }
+
+  return (
+    <div className='App'>
+      <h1>Clothes 'n Things</h1>
+      <nav>
+        {categories.map((category) => (
+          <button key={category} onClick={() => setSelectedCategory(category)}>
+            {category}
+          </button>
+        ))}
+      </nav>
+      <h2>{selectedCategory}</h2>
+      <ul>
+        {!items[selectedCategory]
+          ? null
+          : items[selectedCategory].map((item) => <li key={item}>{item}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+//6 Separate Hooks for Separate Effects
+
+import React, { useState, useEffect } from 'react';
+import { get } from './mockBackend/fetch';
+
+export default function SocialNetwork() {
+  const [menu, setMenu] = useState(null);
+  useEffect(() => {
+    get('/menu').then((response) => {
+      setMenu(response.data);
+    });
+  }, []);
+
+  const [newsFeed, setNewsFeed] = useState(null);
+  useEffect(() => {
+    get('/news-feed').then((response) => {
+      setNewsFeed(response.data);
+    });
+  }, []);
+
+  const [friends, setFriends] = useState(null);
+  useEffect(() => {
+    get('/friends').then((response) => {
+      setFriends(response.data);
+    });
+  }, []);
+
+  return (
+    <div className='App'>
+      <h1>My Network</h1>
+      {!menu ? (
+        <p>Loading..</p>
+      ) : (
+        <nav>
+          {menu.map((menuItem) => (
+            <button key={menuItem}>{menuItem}</button>
+          ))}
+        </nav>
+      )}
+      <div className='content'>
+        {!newsFeed ? (
+          <p>Loading..</p>
+        ) : (
+          <section>
+            {newsFeed.map(({ id, title, message, imgSrc }) => (
+              <article key={id}>
+                <h3>{title}</h3>
+                <p>{message}</p>
+                <img src={imgSrc} alt='' />
+              </article>
+            ))}
+          </section>
+        )}
+        {!friends ? (
+          <p>Loading..</p>
+        ) : (
+          <aside>
+            <ul>
+              {friends
+                .sort((a, b) => (a.isOnline && !b.isOnline ? -1 : 0))
+                .map(({ id, name, isOnline }) => (
+                  <li key={id} className={isOnline ? 'online' : 'offline'}>
+                    {name}
+                  </li>
+                ))}
+            </ul>
+          </aside>
+        )}
+      </div>
+    </div>
+  );
+}
