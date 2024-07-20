c3603ae65e7a30cf83872457ed5605ea20ac186e
sevda <iremberber0@gmail.com>
2024-07-20 18:12:57 +0300

    Update React Programming Patterns.js

--- my-react-app/src/React Programming Patterns.js
+++ my-react-app/src/React Programming Patterns.js
@@ -87,3 +87,51 @@ function GuineaPigsForm({favoriteGP, onSelectFavorite, onResetFavorite}){
         <button onClick={onResetFavorite}>Reset Favorite</button>
 		  </div>
 };
+
+//4 Render Presentational Components in Container Component
+
+import React, { useState, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import GuineaPigsSlideShow from "../components/GuineaPigsSlideShow";
+import GuineaPigsForm from "../components/GuineaPigsForm";
+
+/*const GUINEAPATHS = [
+  "https://content.codecademy.com/courses/React/react_photo-guineapig-1.jpg",
+  "https://content.codecademy.com/courses/React/react_photo-guineapig-2.jpg",
+  "https://content.codecademy.com/courses/React/react_photo-guineapig-3.jpg",
+  "https://content.codecademy.com/courses/React/react_photo-guineapig-4.jpg",
+];*/
+
+function GuineaPigsContainer() {
+  const [currentGP, setCurrentGP] = useState(0);
+	const [favoriteGP, setFavoriteGP] = useState(0);
+	const src = GUINEAPATHS[currentGP];
+
+  const favoriteChangeHandler = (event) => {
+    setFavoriteGP(parseInt(event.target.value));
+  }
+
+  const resetFavoriteHandler = () => {
+    setFavoriteGP(0);
+  }
+
+  useEffect(() => {
+		const intervalId = setInterval(() => {
+			setCurrentGP(prevGP => {
+				const nextGP = prevGP + 1;
+				return nextGP % GUINEAPATHS.length;
+			});
+		}, 5000)
+		return () => clearInterval(intervalId);
+	}, []);
+
+
+	return (
+    <>
+      <GuineaPigsSlideShow src={src} isFavorite={currentGP === favoriteGP}/>
+      <GuineaPigsForm favoriteGP={favoriteGP} onSelectFavorite={favoriteChangeHandler} onResetFavorite={resetFavoriteHandler}/>
+    </>
+  );
+}
+
+//export default GuineaPigsContainer;
