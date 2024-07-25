b701aa8b07a31c756bbba29e8734eac573bc843c
sevda <iremberber0@gmail.com>
2024-07-25 17:53:43 +0300

    Update React Forms.js

--- React Forms.js
+++ React Forms.js
@@ -21,3 +21,48 @@ function Input() {
     </>
   );
 }
+
+//2 Write an Input Event Handler
+
+function handleUserInput(e){
+    setUserInput(e.target.value);
+  }
+
+//3 Set the Input's Initial State
+
+function Input() {
+    const [userInput, setUserInput] = useState('');
+    function handleUserInput(e) {
+      setUserInput(e.target.value);
+    }
+}
+
+//4 Update an Input's Value
+
+function Input() {
+    const [userInput, setUserInput] = useState('');
+    function handleUserInput(e) {
+      setUserInput(e.target.value);
+    }
+    return (
+      <>
+        <div className={styles.emailContainer}>
+          <h2>Let's stay in touch.</h2>
+          <p>
+            Sign up for our newsletter to stay up-to-date on the latest products,
+            receive exclusive discounts, and connect with other programmers who
+            share your passion for all things tech.
+          </p>
+          <form>
+            <label for="email">Email: </label>
+            <input id="email" type="text" onChange={handleUserInput} value={userInput} />
+          </form>
+        </div>
+        <div className={styles.inputDisplay}>
+          <h2>Current User Input: </h2>
+          <h4>{userInput}</h4>
+        </div>
+      </>
+    );
+  }
+  
